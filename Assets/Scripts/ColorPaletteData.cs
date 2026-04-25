7457ea1bbd6dad72b9fdc25ba940d15f7265e5c8
İremnur Yıldız <iremnur@Iremnur-MacBook-Air.local>
2026-04-25 15:46:06 +0300

    scriptable object

---
+++ Assets/Scripts/ColorPaletteData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ColorPalette", menuName = "Game/Color Palette")]
+public class ColorPaletteData : ScriptableObject
+{
+    public Color[] colors;
+}
